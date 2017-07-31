e4a6cde63b47098bcb4c197901c646f37c726364
visantillan <visantillan@adeamexico.com.mx>
2017-07-31 13:34:46 -0500

    spring security added

---
+++ src/main/java/mx/com/adea/projects/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,27 @@
+package mx.com.adea.projects.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Administrador on 31/07/2017.
+ * +
+ */
+@Component( "restAuthenticationEntryPoint" )
+public class RestAuthenticationEntryPoint
+    implements AuthenticationEntryPoint {
+
+  @Override
+  public void commence(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      AuthenticationException authException) throws IOException {
+
+    response.sendError( HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized" );
+  }
+}
